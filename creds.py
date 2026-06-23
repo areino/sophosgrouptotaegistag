1d16420431807678f3c301dbffea54365e080075
areino <areino@users.noreply.github.com>
2026-06-23 19:39:49 +0200

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,9 @@
+sophos_tenant_id = ""
+sophos_client_id = ""
+sophos_client_secret = ""
+sophos_data_region = "us03"
+
+taegis_api_endpoint = "https://api.ctpx.secureworks.com"
+taegis_client_id = ""
+taegis_client_secret = ""
+taegis_tenant_id = ""
